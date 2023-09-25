dc32c7f9de2b53bee4ce79fa27f7eaf95fd02470
Saurabh Pushpam <saurabhpushpam3@gmail.com>
2023-09-25 11:40:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express= require('express');
+const app= express();
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/ecomm");
+
+
+
+// user routes
+
+const user_route= require("./routes/userRoutes");
+
+ app.use('/api', user_route);
+
+// store routes
+
+const store_routes= require("./routes/storeRoutes");
+app.use('/api', store_routes);
+
+
+//catogary routes
+
+const category_routes= require("./routes/categoryRoutes");
+app.use('/api', category_routes);
+
+// sub-catogary routes
+
+const subcategory_route= require("./routes/subCategoryRoutes");
+app.use('/api', subcategory_route);
+
+
+// Add-product routes
+
+const product_routes= require("./routes/productRoutes");
+app.use('/api', product_routes)
+
+
+app.listen(3000, function(){
+    console.log('server is ready');
+});
+
